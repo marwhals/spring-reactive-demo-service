11f9b1a87ee0758582f5a67fe653bd79b50bb05b
marwhals <marjan.mubarok2012@gmail.com>
2025-04-26 14:55:25 +0100

    Setup project

---
+++ src/main/java/com/spring_reactive/spring_reactive_demo_service/SpringReactiveDemoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.spring_reactive.spring_reactive_demo_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveDemoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactiveDemoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_reactive/spring_reactive_demo_service/SpringReactiveDemoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_reactive.spring_reactive_demo_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveDemoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
